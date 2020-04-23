22d7da4eb584124b50f7918b349d65668340f051
Oleksii <Oleksii.lytvynovbohdanov@gmail.com>
2020-04-23 20:29:03 +0300

    Init commit

---
+++ RealityPuzzle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealityPuzzle
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 23.04.2020.
+//  Copyright © 2020 Oleksii Lytvynov-Bohdanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
