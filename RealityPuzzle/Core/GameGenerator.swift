be13776b82e4325e4ca2bfc52a45f093844c2f1e
Oleksii <Oleksii.lytvynovbohdanov@gmail.com>
2020-04-25 16:14:55 +0300

    Added game generator

--- RealityPuzzle/Core/GameGenerator.swift
+++ RealityPuzzle/Core/GameGenerator.swift
@@ -9,5 +9,14 @@
 import Foundation
 
 struct GameGenerator {
-    
+    static func generate(count: Int) -> [Int] {
+        var array = Array(0..<count)
+        var finalArray = [Int]()
+        while array.count > 0 {
+            let randomNumber = Int.random(in: 0..<array.count)
+            finalArray.append(array[randomNumber])
+            array.remove(at: randomNumber)
+        }
+        return finalArray
+    }
 }
