f6f19812f01f54fe998d7372d307dafa684407a8
Oleksii <Oleksii.lytvynovbohdanov@gmail.com>
2020-04-25 16:57:38 +0300

    Added board to the controller

--- RealityPuzzle/Game/Board.swift
+++ RealityPuzzle/Game/Board.swift
@@ -10,4 +10,39 @@ import UIKit
 
 class Board: UIView {
 
+    let distanceBetweenElements: CGFloat = 20
+    let buttonSize: CGFloat = 60
+    
+    var gameWonCallback: (() -> Void)?
+    var userMovedPiece: (() -> Void)?
+    var game: Game?
+    
+//    private func registerGesture(label: UILabel, direction: UISwipeGestureRecognizer.Direction, selector: Selector
+//    ) {
+//        let gesture =
+//            UISwipeGestureRecognizer(target: self,
+//                                     action: selector)
+//        gesture.direction = direction
+//        label.addGestureRecognizer(gesture)
+//    }
+    
+    private func registerGestures(for label: UILabel) {
+        
+//        registerGesture(label: label, direction: .left, selector: #selector(handleGesture))
+//        registerGesture(label: label, direction: .right, selector: #selector(handleGesture))
+//        registerGesture(label: label, direction: .up, selector: #selector(handleGesture))
+//        registerGesture(label: label, direction: .down, selector: #selector(handleGesture))
+        
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(handleTapGesture))
+        label.addGestureRecognizer(tap)
+    }
+    
+    @objc private func handleTapGesture(gesture: UISwipeGestureRecognizer) -> Void {
+        guard let piece = gesture.view as? Piece else { return }
+//        if game.movePiceIfPossible(piece) {
+            
+//        }
+        // TOdO: Move to blank if possible
+    }
 }
--- RealityPuzzle/Game/Game.swift
+++ RealityPuzzle/Game/Game.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 struct Game {
+    let pieces: [Piece]
+    let blankPiece: Piece
+    let gameLength: Int
     
+    private enum PiecePosition {
+        case left
+        case right
+        case top
+        case bottom
+        case equal
+    }
+    
+    func movePiceIfPossible(_ piece: Piece) {
+        
+    }
 }
