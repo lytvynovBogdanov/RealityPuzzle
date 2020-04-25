49cf4bfaa0495cdb2f0612507580c1b8189e5008
Oleksii <Oleksii.lytvynovbohdanov@gmail.com>
2020-04-25 14:14:59 +0300

    Added Piece code

--- RealityPuzzle/Game/Piece.swift
+++ RealityPuzzle/Game/Piece.swift
@@ -9,5 +9,32 @@
 import UIKit
 
 class Piece: UILabel {
-
+    var coordinate = (0, 0)
+    let finalCoordinate: (Int, Int)
+    
+    var isFinalPosition: Bool {
+        return coordinate == finalCoordinate
+    }
+    
+    convenience init(coordinate: (Int, Int), finalPosition: (Int, Int)) {
+        self.init(frame: CGRect.zero, title: "", finalPosition: finalPosition)
+        self.coordinate = coordinate
+    }
+    
+    init(frame: CGRect, title: String, finalPosition: (Int, Int)) {
+        self.finalCoordinate = finalPosition
+        super.init(frame: frame)
+        self.text = title
+        self.backgroundColor = .orange
+        self.textColor = .white
+        self.font = UIFont.systemFont(ofSize: 23)
+        self.textAlignment = .center
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.finalCoordinate = (0, 0)
+        super.init(coder: aDecoder)
+    }
 }
